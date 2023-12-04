468a1b769b72f9864e53ab6142616b32e1469cda
Confidence Tshikovhi <136083432+vetroue@users.noreply.github.com>
2023-12-04 15:11:18 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    from sys import argv
-
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    numb1 = int(argv[1])
-    numb2 = int(argv[3])
-
-    if argv[2] not in ["+", "-", "*", "/"]:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    elif argv[2] is "+":
-        print("{} + {} = {}".format(numb1, numb2, add(numb1, numb2)))
-    elif argv[2] is "-":
-        print("{} - {} = {}".format(numb1, numb2, sub(numb1, numb2)))
-    elif argv[2] is "*":
-        print("{} * {} = {}".format(numb1, numb2, mul(numb1, numb2)))
-    elif argv[2] is "/":
-        print("{} / {} = {}".format(numb1, numb2, div(numb1, numb2)))
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
