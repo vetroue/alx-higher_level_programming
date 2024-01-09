5ed11b7b7fc3a9ddfa1c1bd1ec15f2a23567662d
Confidence Tshikovhi <136083432+vetroue@users.noreply.github.com>
2024-01-09 16:43:10 +0200

    Delete 0x08-python-more_classes/0x09-python-everything_is_object directory

--- 0x08-python-more_classes/0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(num=[]) -> str:
-    num.append("BestSchool")
-    return ", ".join(num)
--- 0x08-python-more_classes/0x09-python-everything_is_object/19-copy_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def copy_list(mm):
-    return mm.copy()
